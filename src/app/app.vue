bb04d137b4739138dd58c4db4f82e50fbf24d90c
wanghao <wanghao@ninghao.net>
2020-10-05 16:22:44 +0800

    v-if：按条件显示内容

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,37 +1,22 @@
 <template>
   <h3>{{ name }}</h3>
-  <div v-for="(post, index) in postList" :key="post.id">
-    {{ index + 1 }} {{ post.content }} - <small>{{ post.author }}</small>
-  </div>
+  <div v-if="visible">隐藏的内容！</div>
+  <button @click="visible = !visible">{{ visible ? '隐藏' : '显示' }}</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: 'NINGHAO',
-
-      postList: [
-        {
-          id: 1,
-          content: '故人西辞黄鹤楼，烟花三月下扬州',
-          author: '李白',
-        },
-        {
-          id: 2,
-          content: '好雨知时节，当春乃发生',
-          author: '杜甫',
-        },
-        {
-          id: 3,
-          content: '浔阳江头夜送客，枫叶荻花秋瑟瑟',
-          author: '白居易',
-        },
-      ],
+      name: '宁皓网',
+      visible: false,
     };
   },
 };
 </script>
 
 <style>
+div {
+  margin: 8px 0;
+}
 </style>
