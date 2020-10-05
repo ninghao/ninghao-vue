db56914b576fe469fa0a47c5937eea7dcc0cef51
wanghao <wanghao@ninghao.net>
2020-10-05 12:02:36 +0800

    创建 Vue 应用实例

---
+++ src/app/app.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>NINGHAO</h3>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import App from './app/app.vue';
+
+/**
+ * 创建应用
+ */
+const app = createApp(App);
+
+/**
+ * 挂载应用
+ */
+app.mount('#app');
