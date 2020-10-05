ef8ce196259990185deaaf0a42906e4d4ae162c9
wanghao <wanghao@ninghao.net>
2020-10-05 16:48:09 +0800

    绑定 Class

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,7 +1,15 @@
 <template>
   <h3>{{ name }}</h3>
-  <div v-if="visible">隐藏的内容！</div>
-  <button @click="visible = !visible">{{ visible ? '隐藏' : '显示' }}</button>
+  <div class="menu">
+    <div
+      :class="['menu-item', { active: currentItem === index }]"
+      @click="currentItem = index"
+      v-for="(item, index) in menuItems"
+      :key="index"
+    >
+      {{ item }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,14 +17,20 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      visible: false,
+      menuItems: ['首页', '热门', '发布'],
+      currentItem: 0,
     };
   },
 };
 </script>
 
 <style>
-div {
-  margin: 8px 0;
+.menu {
+  display: flex;
+  gap: 16px;
+}
+
+.active {
+  color: #6435c9;
 }
 </style>
