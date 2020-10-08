9b028fb642d2b7e753f01867f56db2bbe8abfba6
wanghao <wanghao@ninghao.net>
2020-10-08 13:49:02 +0800

    单选按钮（Radio）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,18 +1,24 @@
 <template>
   <h3>{{ name }}</h3>
-  <label for="tag-1">
-    <input id="tag-1" type="checkbox" value="大明湖" v-model="tags" />
-    大明湖
+  <label for="subscription-annual">
+    <input
+      id="subscription-annual"
+      type="radio"
+      value="annual"
+      v-model="subscription"
+    />
+    一年
   </label>
-  <label for="tag-2">
-    <input id="tag-2" type="checkbox" value="黑虎泉" v-model="tags" />
-    黑虎泉
+  <label for="subscription-quarterly">
+    <input
+      id="subscription-quarterly"
+      type="radio"
+      value="quarterly"
+      v-model="subscription"
+    />
+    季度
   </label>
-  <label for="tag-3">
-    <input id="tag-3" type="checkbox" value="趵突泉" v-model="tags" />
-    趵突泉
-  </label>
-  <span>→ Tags: {{ tags }}</span>
+  <span>→ Subscription: {{ subscription }}</span>
 </template>
 
 <script>
@@ -20,7 +26,7 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      tags: [],
+      subscription: 'annual',
     };
   },
 };
