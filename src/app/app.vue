fc2e41f7c96efc305ad55a9a483a5d8019f2fba8
wanghao <wanghao@ninghao.net>
2020-10-08 21:24:19 +0800

    定义与使用 Vue 组件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,14 +1,22 @@
 <template>
   <h3>{{ name }}</h3>
+  <AppButton></AppButton>
+  <AppButton />
+  <app-button></app-button>
+  <app-button />
 </template>
 
 <script>
+import AppButton from './components/app-button.vue';
+
 export default {
   data() {
     return {
       name: '宁皓网',
     };
   },
+
+  components: { AppButton },
 };
 </script>
 
