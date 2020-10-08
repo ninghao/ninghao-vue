b142512753ec84564b7879af33688280a22ae052
wanghao <wanghao@ninghao.net>
2020-10-08 14:03:17 +0800

    change 事件：用户提交修改元素值时发生的事件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
-  <input type="text" @input="onInputContent" />
+  <input type="text" @change="onChangeContent" />
 </template>
 
 <script>
@@ -12,8 +12,8 @@ export default {
   },
 
   methods: {
-    onInputContent(event) {
-      console.log(event);
+    onChangeContent(event) {
+      console.log(event.target.value);
     },
   },
 };
