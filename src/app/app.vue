d548a467de76f26a3bd7abdaaa5e8dafc8b7a039
wanghao <wanghao@ninghao.net>
2020-10-08 15:42:49 +0800

    v-model 的修饰符：.trim

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
-  <input type="text" v-model.number="content" @change="onChangeContent" />
+  <input type="text" v-model.trim="content" @change="onChangeContent" />
   <span>→ Content: {{ content }}</span>
 </template>
 
@@ -9,17 +9,13 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      content: 0,
+      content: '',
     };
-  },
-
-  created() {
-    console.log('content 数据的类型（组件创建时）：', typeof this.content);
   },
 
   methods: {
     onChangeContent() {
-      console.log('content 数据的类型（元素值变化后）：', typeof this.content);
+      console.log(this.content, this.content.length);
     },
   },
 };
