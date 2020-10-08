df25472b3c387d7dbac858bd8dabaa23431753a2
wanghao <wanghao@ninghao.net>
2020-10-08 12:59:43 +0800

    清理

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,15 +1,5 @@
 <template>
   <h3>{{ name }}</h3>
-  <div class="menu">
-    <div
-      :class="['menu-item', { active: currentItem === index }]"
-      @click="currentItem = index"
-      v-for="(item, index) in menuItems"
-      :key="index"
-    >
-      {{ item }}
-    </div>
-  </div>
 </template>
 
 <script>
@@ -17,8 +7,6 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      menuItems: ['首页', '热门', '发布'],
-      currentItem: 0,
     };
   },
 };
