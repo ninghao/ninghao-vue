24a864170e9114f385bb716b7e1b3bf7b03c2c2a
wanghao <wanghao@ninghao.net>
2020-10-08 13:42:47 +0800

    多值复选框（Checkbox）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,7 +1,18 @@
 <template>
   <h3>{{ name }}</h3>
-  <input id="checked" type="checkbox" v-model="checked" />
-  <label for="checked">→ Checked: {{ checked }}</label>
+  <label for="tag-1">
+    <input id="tag-1" type="checkbox" value="大明湖" v-model="tags" />
+    大明湖
+  </label>
+  <label for="tag-2">
+    <input id="tag-2" type="checkbox" value="黑虎泉" v-model="tags" />
+    黑虎泉
+  </label>
+  <label for="tag-3">
+    <input id="tag-3" type="checkbox" value="趵突泉" v-model="tags" />
+    趵突泉
+  </label>
+  <span>→ Tags: {{ tags }}</span>
 </template>
 
 <script>
@@ -9,7 +20,7 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      checked: true,
+      tags: [],
     };
   },
 };
