078817390b51522b93ea5c0747810eada8229f0c
wanghao <wanghao@ninghao.net>
2020-10-08 13:59:43 +0800

    input 事件：元素值变化时发生的事件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,11 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
-  <select v-model="category">
-    <option value="">- 选择分类 -</option>
-    <option value="development">应用开发</option>
-    <option value="game">游戏开发</option>
-  </select>
-  <span>→ Category: {{ category }}</span>
+  <input type="text" @input="onInputContent" />
 </template>
 
 <script>
@@ -13,8 +8,13 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      category: '',
     };
+  },
+
+  methods: {
+    onInputContent(event) {
+      console.log(event);
+    },
   },
 };
 </script>
