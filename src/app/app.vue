fb60f6f8fd60ae6c7bc86a31a2508ebee2f1bdf9
wanghao <wanghao@ninghao.net>
2020-10-08 13:52:25 +0800

    选择列表（Select）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,24 +1,11 @@
 <template>
   <h3>{{ name }}</h3>
-  <label for="subscription-annual">
-    <input
-      id="subscription-annual"
-      type="radio"
-      value="annual"
-      v-model="subscription"
-    />
-    一年
-  </label>
-  <label for="subscription-quarterly">
-    <input
-      id="subscription-quarterly"
-      type="radio"
-      value="quarterly"
-      v-model="subscription"
-    />
-    季度
-  </label>
-  <span>→ Subscription: {{ subscription }}</span>
+  <select v-model="category">
+    <option value="">- 选择分类 -</option>
+    <option value="development">应用开发</option>
+    <option value="game">游戏开发</option>
+  </select>
+  <span>→ Category: {{ category }}</span>
 </template>
 
 <script>
@@ -26,7 +13,7 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      subscription: 'annual',
+      category: '',
     };
   },
 };
