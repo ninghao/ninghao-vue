41412e9f1c0f3b728f0c89808be374370bca08d9
wanghao <wanghao@ninghao.net>
2020-10-08 21:26:40 +0800

    Vue 组件的样式

--- src/app/components/app-button.vue
+++ src/app/components/app-button.vue
@@ -1,3 +1,20 @@
 <template>
-  <button>按钮</button>
+  <button class="button">按钮</button>
 </template>
+
+<style>
+.button {
+  outline: none;
+  border: none;
+  background: #e1e1e1;
+  color: #5a5a5a;
+  padding: 8px 16px;
+  margin: 4px;
+  cursor: pointer;
+  border-radius: 3px;
+}
+
+.button:active {
+  opacity: 0.8;
+}
+</style>
