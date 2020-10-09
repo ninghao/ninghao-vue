29116d03a9562c489dc7ae664b26fc7b1acc291f
wanghao <wanghao@ninghao.net>
2020-10-09 09:45:59 +0800

    $refs：引用模板

--- src/app/components/user-login.vue
+++ src/app/components/user-login.vue
@@ -1,5 +1,14 @@
 <template>
   <div>用户登录</div>
-  <input type="text" />
+  <input type="text" ref="username" />
   <input type="text" />
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$refs.username);
+    this.$refs.username.focus();
+  },
+};
+</script>
