428c27613a56c83f2dd48568b513516425134bc8
wanghao <wanghao@ninghao.net>
2020-10-09 16:18:23 +0800

    router-link：路由链接

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,16 @@
 <template>
-  <h3>{{ name }}</h3>
-  <router-view></router-view>
+  <div class="page">
+    <div class="page-header">
+      <h3 class="brand">{{ name }}</h3>
+      <div class="menu">
+        <router-link class="menu-item" to="/">首页</router-link>
+        <router-link class="menu-item" to="/about">关于</router-link>
+      </div>
+    </div>
+    <div class="page-body">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
