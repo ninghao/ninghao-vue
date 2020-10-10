cc56eebcc5d68374174000768a40f0676f59f5b9
wanghao <wanghao@ninghao.net>
2020-10-10 08:24:41 +0800

    动态路由（带地址参数的路由）

--- src/post/post.routes.ts
+++ src/post/post.routes.ts
@@ -1,5 +1,6 @@
 import { RouteRecordRaw } from 'vue-router';
 import PostIndex from './index/post-index.vue';
+import PostShow from './show/post-show.vue';
 
 /**
  * 定义路由
@@ -9,6 +10,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/posts',
     name: 'postIndex',
     component: PostIndex,
+  },
+  {
+    path: '/posts/:postId',
+    name: 'postShow',
+    component: PostShow,
   },
 ];
 
---
+++ src/post/show/post-show.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>PostShow {{ $route.params.postId }}</div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$route.params.postId);
+  },
+};
+</script>
