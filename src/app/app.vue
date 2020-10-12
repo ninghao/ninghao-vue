6d67535c1f97a12544bd58ccc14b3b6d7f28bc13
wanghao <wanghao@ninghao.net>
2020-10-12 20:03:05 +0800

    transition：进入与离开动画

--- src/app/app.vue
+++ src/app/app.vue
@@ -6,8 +6,8 @@
         <div class="card-subtitle">Transition & Animation</div>
       </div>
       <div class="card__content">
-        <transition name="slide">
-          <div v-if="isActive" class="emoji">🌴</div>
+        <transition name="pulse">
+          <div v-if="isActive" class="emoji">👻</div>
         </transition>
       </div>
       <div class="card__action">
