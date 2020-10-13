e4819928c4319e4e60d0104836552dde63a2a0bc
wanghao <wanghao@ninghao.net>
2020-10-13 14:08:29 +0800

    声明具有反应特性的数据（reactive）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,18 +1,26 @@
 <template>
   <h3 @click="name = 'NINGHAO'">{{ name }}</h3>
+  <div @click="user.name = 'WANGHAO'">{{ user.name }}</div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 export default {
   setup() {
     const name = ref('宁皓网');
 
+    const user = reactive({
+      name: '王皓',
+    });
+
+    console.log(user);
+
     console.log(name.value);
 
     return {
       name,
+      user,
     };
   },
 };
