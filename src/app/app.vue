c912b6ffd5b0a9b9696b89abcc2e332e9bab5c11
wanghao <wanghao@ninghao.net>
2020-10-13 13:03:21 +0800

    理解 Vue 的反应性系统

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <h3>{{ name }}</h3>
+  <h3 @click="name = 'NINGHAO'">{{ name }}</h3>
 </template>
 
 <script>
@@ -11,35 +11,7 @@ export default {
   },
 
   created() {
-    const user = {
-      name: '宁皓网',
-    };
-
-    const handler = {
-      get(target, property) {
-        return target[property];
-      },
-
-      set(target, property, value) {
-        if (property === 'name') {
-          if (value.length > 10) {
-            throw new Error('名字太长了！');
-          }
-        }
-
-        return Reflect.set(target, property, value);
-      },
-    };
-
-    const userProxy = new Proxy(user, handler);
-
-    try {
-      userProxy.name = 'NINGHAONINGHAO';
-    } catch (error) {
-      console.log('错误：', error.message);
-    }
-
-    console.log(userProxy.name);
+    console.log(this.$data);
   },
 };
 </script>
