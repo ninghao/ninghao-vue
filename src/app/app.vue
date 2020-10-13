e430d085a25737d90afae04880e0afdb68bc7e17
wanghao <wanghao@ninghao.net>
2020-10-13 16:20:39 +0800

    组合式接口：监视数据（watch）

--- src/app/app.vue
+++ src/app/app.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 
 export default {
   setup() {
@@ -16,6 +16,11 @@ export default {
     // 生命周期
     onMounted(() => {
       console.log('mounted');
+    });
+
+    // 监视数据
+    watch(name, (newName, oldName) => {
+      console.log(newName, oldName);
     });
 
     return {
