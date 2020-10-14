d8fd5e00eda2634904f2c9095b5f893b9189f71b
wanghao <wanghao@ninghao.net>
2020-10-14 14:12:57 +0800

    axios 的请求与响应的拦截器（Interceptors）

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,4 +2,32 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
+/**
+ * 请求拦截器
+ */
+axios.interceptors.request.use(
+  config => {
+    console.log('axios 请求拦截器', config);
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+/**
+ * 响应拦截器
+ */
+axios.interceptors.response.use(
+  response => {
+    console.log('axios 响应拦截器', response);
+
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
 export { axios };
