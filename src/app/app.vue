f68ca241a170672f7a8c3327dae7a30c87c34171
wanghao <wanghao@ninghao.net>
2020-10-14 10:52:29 +0800

    安装与使用 HTTP  客户端请求服务端接口（axios）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,13 +1,27 @@
 <template>
   <h3>{{ name }}</h3>
+  <div v-for="post in posts" :key="post.id">
+    {{ post.title }} -
+    <small>{{ post.user.name }}</small>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       name: '宁皓网',
+      posts: [],
     };
+  },
+
+  created() {
+    axios.get('http://localhost:3000/posts').then(response => {
+      console.log(response);
+      this.posts = response.data;
+    });
   },
 };
 </script>
