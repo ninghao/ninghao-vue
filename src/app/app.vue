01b2a29ca2a827b9a991c1a05df8f79dac536756
wanghao <wanghao@ninghao.net>
2020-10-14 10:56:28 +0800

    处理在 Vue 应用里使用 axios 发送请求时发生的错误

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
+  <div>{{ errorMessage }}</div>
   <div v-for="post in posts" :key="post.id">
     {{ post.title }} -
     <small>{{ post.user.name }}</small>
@@ -14,14 +15,23 @@ export default {
     return {
       name: '宁皓网',
       posts: [],
+      errorMessage: '',
     };
   },
 
   created() {
-    axios.get('http://localhost:3000/posts').then(response => {
-      console.log(response);
-      this.posts = response.data;
-    });
+    axios
+      .get('http://localhost:3000/posts1')
+      .then(response => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch(error => {
+        console.log(error.message);
+        console.log(error.response);
+
+        this.errorMessage = error.message;
+      });
   },
 };
 </script>
