1dcaf308fe10c2e9a26bb64d64aa04c419714bb9
wanghao <wanghao@ninghao.net>
2020-10-19 13:19:11 +0800

    Vue 应用的环境变量配置

---
+++ src/app/app.config.ts
@@ -0,0 +1,4 @@
+/**
+ * 服务接口基本地址
+ */
+export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { API_BASE_URL } from './app.config';
 
 /**
  * HTTP 客户端
  */
 export const apiHttpClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: API_BASE_URL,
 });
