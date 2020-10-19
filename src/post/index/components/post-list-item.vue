584792c68a63748bc4817215cc1ba8f99178fd3c
wanghao <wanghao@ninghao.net>
2020-10-19 14:04:48 +0800

    请求服务接口获取组件需要的数据

--- src/post/index/components/post-list-item.vue
+++ src/post/index/components/post-list-item.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>PostListItem</div>
+  <h3>
+    {{ item.title }} - <small>{{ item.user.name }}</small>
+  </h3>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  props: {
+    item: Object,
+  },
+});
 </script>
--- src/post/index/components/post-list.vue
+++ src/post/index/components/post-list.vue
@@ -1,12 +1,24 @@
 <template>
-  <PostListItem />
+  <PostListItem v-for="post in posts" :item="post" :key="post.id" />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import PostListItem from './post-list-item';
+import { apiHttpClient } from '@/app/app.service';
 
 export default defineComponent({
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  async created() {
+    const response = await apiHttpClient.get('/posts');
+    this.posts = response.data;
+  },
+
   components: {
     PostListItem,
   },
